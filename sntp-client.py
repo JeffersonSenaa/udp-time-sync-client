71e92c161f574ffd29a8a7b8346e6c58991918a9
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-11-27 14:01:11 -0300

    Função para interpretação da resposta em formato Data/hora

--- sntp-client.py
+++ sntp-client.py
@@ -8,6 +8,12 @@ PORT = 123
 TIMEOUT = 20
 MSG_SNTP = b'\x1b' + 47 * b'\0'
 
+def interpretar_resposta(data):
+    # Extrai o timestamp transmitido (txTm_s) - começa no byte 40
+    txTm_s = struct.unpack("!12I", data)[10] 
+    # Conversao de epoch SNTP (1900) para epoch Unix (1970)
+    timestamp = txTm_s - 2208988800  
+    return datetime.utcfromtimestamp(timestamp).strftime('%a %b %d %H:%M:%S %Y')
 
 def consultar_servidor_ntp():
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -15,7 +21,7 @@ def consultar_servidor_ntp():
             try:
                 s.sendto(MSG_SNTP, (NTP_SERVER, PORT))
                 data, _ = s.recvfrom(1024)
-                return data
+                return interpretar_resposta(data)
             except socket.timeout:
                 return None
         
