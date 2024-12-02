d3658d72affc7e7b9d024023de61fb8cb4441fb4
JeffersonSenaa <jeffersonsena12144@gmail.com>
2024-12-02 14:12:27 -0300

    Ajustando fuso horário da resposta com fuso horário America/Sao_Paulo

--- sntp-client.py
+++ sntp-client.py
@@ -1,19 +1,20 @@
 import socket
-import time
 import struct
-from datetime import datetime
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 
 NTP_SERVER = "pool.ntp.org" #Servidor NTP público
 PORT = 123
 TIMEOUT = 20
 MSG_SNTP = b'\x1b' + 47 * b'\0'
+# Calcula a diferença entre as épocas SNTP (1900) e Unix (1970)
+EPOCH_SNTPS_TO_UNIX = (datetime(1970, 1, 1, tzinfo=timezone.utc) - datetime(1900, 1, 1, tzinfo=timezone.utc)).total_seconds()
 
 def interpretar_resposta(data):
     # Extrai o timestamp transmitido (txTm_s) - começa no byte 40
     txTm_s = struct.unpack("!12I", data)[10] 
-    # Conversao de epoch SNTP (1900) para epoch Unix (1970)
-    timestamp = txTm_s - 2208988800  
-    return datetime.utcfromtimestamp(timestamp).strftime('%a %b %d %H:%M:%S %Y')
+    timestamp = txTm_s - EPOCH_SNTPS_TO_UNIX  
+    return datetime.fromtimestamp(timestamp, timezone.utc).astimezone(ZoneInfo("America/Sao_Paulo")).strftime('%a %b %d %H:%M:%S %Y')
 
 def consultar_servidor_ntp():
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -31,6 +32,6 @@ for tentativa in range(2):
         print(f'Data/hora: {resultado}')
         break
     else:
-        print("Tentativa falhou. Tentando novamente...")
+        print("Tentativa falhou. Segunda tentativa...")
 else:
         print("Data/hora: não foi possível contactar servidor")
